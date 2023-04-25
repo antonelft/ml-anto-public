eac4c997eda52c6a1afefa3d728efd3b7d1735b2
antonelft <47756946+antonelft@users.noreply.github.com>
2023-04-25 10:33:31 +0100

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function destruir() {
+  document.body.style.backgroundColor = 'blue';
+  document.getElementById("gh").style.color = "red";
+  document.getElementById("gh").innerHTML = "Hola Chicas"
+  document.getElementById("mojitoparachicas").style.display = "inline";
+}   
